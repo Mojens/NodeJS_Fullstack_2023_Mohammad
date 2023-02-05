aa53f3ee7d4e31e9f9d2b17c4392ead38275934b
mojens <moha135h@stud.kea.dk>
2023-02-05 16:14:06 +0100

    done with excersise 3

--- 00._Miscellaneous/1._Intro_Exercises/3.Objects.js
+++ 00._Miscellaneous/1._Intro_Exercises/3.Objects.js
@@ -7,10 +7,17 @@ const myObj = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
 
+console.log(myObj.message);
+
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
+
+const mySelf = {
+    name: "Mohammad",
+    age: 22
+};
 
 
 // --------------------------------------
@@ -20,6 +27,8 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
@@ -27,6 +36,8 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+thisSong.about = "Just a tribute.";
 
 // --------------------------------------
 
