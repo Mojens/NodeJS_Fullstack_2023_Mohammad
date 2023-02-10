f3ad506ae169ddb5b04bb83ea309863f9eeb923b
mojens <moha135h@stud.kea.dk>
2023-02-10 11:24:31 +0100

    Different endpoints

--- 01._First_server/app.js
+++ 01._First_server/app.js
@@ -23,11 +23,29 @@ let dinosaurKick = 0;
 app.get("/kickthedinosaur", (req, res) => {
     dinosaurKick+=1;
     res.send({ message: `People have kicked the dinosaur ${dinosaurKick} times`,
-    dinosaur: "ow ow ow" 
+    dinosaur: "ow ow ow" });
 });
+
+app.get("/about", (req, res) => {
+    res.send(`
+    <h1> About us </h1>
+    <h2> We are a group of people who love dinosaurs!</h2>
+    `);
 });
 
 
+// bat?adjective=spooky
+app.get("/bat", (req, res) => {
+    console.log(req.query);
+
+    res.send({ message: `The bat is ${req.query.adjective}`})
+});
+
+// bottle/large
+app.get("/bottle/:bottleSize", (req, res) => {
+    console.log(req.params);
+    res.send({ message: `The bottle is ${req.params.bottleSize}`});
+});
 
 
 app.listen(8080); //open the port 8080 to use for our server 8080 beacuse its production and http
