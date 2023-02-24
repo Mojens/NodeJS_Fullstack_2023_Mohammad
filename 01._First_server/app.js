4141ce2194130b3c3832728521791ee40d8ae5ae
mojens <moha135h@stud.kea.dk>
2023-02-24 11:42:10 +0100

    time

--- 01._First_server/app.js
+++ 01._First_server/app.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 
 let bicycleSpin = 0;
 app.get("/spinthebicycle", (req, res) => {
-    bicycleSpin+=1;
+    bicycleSpin += 1;
     res.send({ message: `People have spun the bicycle wheel ${bicycleSpin} times` });
 });
 
@@ -23,9 +23,11 @@ app.get("/spinthebicycle", (req, res) => {
     */
 let dinosaurKick = 0;
 app.get("/kickthedinosaur", (req, res) => {
-    dinosaurKick+=1;
-    res.send({ message: `People have kicked the dinosaur ${dinosaurKick} times`,
-    dinosaur: "ow ow ow" });
+    dinosaurKick += 1;
+    res.send({
+        message: `People have kicked the dinosaur ${dinosaurKick} times`,
+        dinosaur: "ow ow ow"
+    });
 });
 
 app.get("/about", (req, res) => {
@@ -40,19 +42,46 @@ app.get("/about", (req, res) => {
 app.get("/bat", (req, res) => {
     console.log(req.query);
 
-    res.send({ message: `The bat is ${req.query.adjective}`})
+    res.send({ message: `The bat is ${req.query.adjective}` })
 });
 
 // bottle/large
 app.get("/bottle/:bottleSize", (req, res) => {
     console.log(req.params);
-    res.send({ message: `The bottle is ${req.params.bottleSize}`});
+    res.send({ message: `The bottle is ${req.params.bottleSize}` });
 });
 
 app.post("/package", (req, res) => {
     console.log(req.body);
-    res.send({ message: req.body});
+    res.send({ message: req.body });
 });
 
+/* Time */
+app.get("/time", (req, res) => {
+
+    res.send({
+        timeUTC: new Date(),
+        timeLocal: Date(),
+        timeUnix: Date.now()
+    })
+});
+
+
+
+
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+app.get("/time/day", (req, res) => {
+    // new Date().toLocaleDateString("da-dk",{weekday: "long"})
+    res.send({ Day: weekday[new Date().getDay()] });
+
+});
+
+const month = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+
+app.get("/time/month", (req, res) => {
+    res.send({ Month: month[new Date().getMonth()] });
+});
 
 app.listen(8080); //open the port 8080 to use for our server 8080 beacuse its production and http
