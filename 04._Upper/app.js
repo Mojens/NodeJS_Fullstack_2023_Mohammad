6bdc34dd03d3b64e611cd3600d714654d050cd7e
mojens <moha135h@stud.kea.dk>
2023-03-17 09:32:56 +0100

    Server rendering

--- 04._Upper/app.js
+++ 04._Upper/app.js
@@ -5,19 +5,36 @@ const PORT = 8080;
 
 app.use(express.static("public")); // need this for making css and js work inside of the html files
 
-import jokes from "./util/jokes.js"
-console.log(await jokes.getJoke());
+// import jokes from "./util/jokes.js";
+
+import fs from "fs";
+// Components
+const navbar = fs.readFileSync("./public/components/navbar/navbar.html").toString();
+const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
+
+
+// Pages
+const frontpage = fs.readFileSync("./public/pages/frontpage/frontpage.html").toString();
+const IRLQuests = fs.readFileSync("./public/pages/IRLQuests/IRLQuests.html").toString();
+const jokes = fs.readFileSync("./public/pages/jokes/jokes.html").toString();
+
+// Constructed pages    
+const frontpagePage = navbar + frontpage + footer;
+const IRLQuestsPage = navbar + IRLQuests + footer;
+const jokesPage = navbar + jokes + footer;
 
 app.get("/", (req, res) => {    
-    res.sendFile(path.resolve("public/pages/frontpage/frontpage.html"))
+    // readFileSync is a blocking function, so it will wait for the file to be read before continuing
+    // res.sendFile(path.resolve("public/pages/frontpage/frontpage.html"))
+    res.send(frontpagePage);
 });
 
 app.get("/IRLQuests", (req, res) => {
-    res.sendFile(path.resolve("public/pages/IRLQuests/IRLQuests.html"))
+    res.send(IRLQuestsPage);
 });
 
 app.get("/jokes", (req,res) =>{
-    res.sendFile(path.resolve("public/pages/jokes/jokes.html"))
+    res.send(jokesPage);
 });
 
 
