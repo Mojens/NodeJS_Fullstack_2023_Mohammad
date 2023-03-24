c54092a2db49096fadb81aa976f43a143b78506a
mojens <moha135h@stud.kea.dk>
2023-03-24 11:08:12 +0100

    first svelte project

---
+++ 05._First_Svelte_Project/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Greeter from "./lib/Greeter.svelte";
+
+</script>
+
+<Greeter name="Martin"/>
+<Greeter name="Mohammad"/>
+<Greeter name="Dannie"/>
+<Greeter name="Daniel"/>
---
+++ 05._First_Svelte_Project/src/lib/Greeter.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let name = "Martin";
+    let greeting = "Hello";
+
+    function changeGreeting() {
+        if (greeting === "Hello") {
+            greeting = "Goodbye";
+        } else {
+            greeting = "Hello";
+        }
+    }
+</script>
+
+<h1 on:click={changeGreeting} on:keydown={changeGreeting}>
+    {greeting}
+    {name}.
+</h1>
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
