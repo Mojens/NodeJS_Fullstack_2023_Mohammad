679a4203e8e14258f5bd2b59e4232e0ee117de63
mojens <moha135h@stud.kea.dk>
2023-03-24 09:29:48 +0100

    fetch different type of activity

--- 04._Upper/public/pages/IRLQuests/IRLquests.js
+++ 04._Upper/public/pages/IRLQuests/IRLquests.js
@@ -1,6 +1,16 @@
-fetch('http://www.boredapi.com/api/activity/')
-.then(res => res.json())
-.then(data => {
-   document.getElementById("quest")
-    .innerText = data.activity
-})
+function fetchQuest(activityQueryString = "") {
+    fetch(`http://www.boredapi.com/api/activity/${activityQueryString}`)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("quest")
+                .innerText = data.activity
+        })
+}
+fetchQuest()
+
+document.getElementById("quest-type-btn").addEventListener("click", getNewQuest)
+
+function getNewQuest() {
+    const dropwDownValue = document.getElementById("activity-select").value
+    fetchQuest(`?type=${dropwDownValue}`)
+}
