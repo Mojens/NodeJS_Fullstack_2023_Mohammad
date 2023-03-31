dcd2c747a08fba85b5d90e062930f4842302ac73
mojens <moha135h@stud.kea.dk>
2023-03-31 09:51:04 +0200

    styling

--- 06._svelte-family/src/components/Nibling/Nibling.svelte
+++ 06._svelte-family/src/components/Nibling/Nibling.svelte
@@ -1,9 +1,37 @@
 <script>
     export let nibling;
+    
 </script>
 
-{#if nibling.isGirl}
-    <p>Niece: {nibling.name}</p>
-{:else}
-    <p>Nephew: {nibling.name}</p>
-{/if}
+<div 
+class:is-girl={nibling.isGirl} 
+class:is-boy={!nibling.isGirl} 
+class={nibling.blackSheep || "not-a-black-sheep"}>
+    {#if nibling.isGirl}
+        <p>Niece: {nibling.name}</p>
+    {:else}
+        <p>Nephew: {nibling.name}</p>
+    {/if}
+</div>
+
+<style>
+    .is-girl{
+        border: 3px solid pink;
+    }
+    .is-boy{
+        border: 3px solid blue;
+    }
+    .medium-black-sheep {
+        background-color: burlywood;
+        color: white;
+    }
+    .ultra-black-sheep {
+        background-color: brown;
+        color: white;
+        font-weight: bold;
+    }
+    .not-a-black-sheep {
+        background-color: aliceblue;
+        color: black;
+    }
+</style>
