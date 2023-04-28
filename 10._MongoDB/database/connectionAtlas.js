73fbdd741c5ee21db8cac948831756b69419120b
mojens <moha135h@stud.kea.dk>
2023-04-28 11:45:35 +0200

    combine with cloud db

---
+++ 10._MongoDB/database/connectionAtlas.js
@@ -0,0 +1,28 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv"
+dotenv.config();
+
+const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@candy-clusterr.wvz2z2a.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
